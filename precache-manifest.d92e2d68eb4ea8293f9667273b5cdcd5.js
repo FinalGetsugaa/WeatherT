ddf2d4417becc61d387aeccecd9dac75cfbd8485
Anant <joshianant34@gmail.com>
2020-04-20 17:48:29 +0530

    Updates

---
+++ precache-manifest.d92e2d68eb4ea8293f9667273b5cdcd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016d6ebd1220a7ac0b579c7cc7894426",
+    "url": "/WeatherT.github.io/index.html"
+  },
+  {
+    "revision": "ff715af70a4f749a6895",
+    "url": "/WeatherT.github.io/static/css/main.018b9cdc.chunk.css"
+  },
+  {
+    "revision": "a2a29b7117987b2d9e1e",
+    "url": "/WeatherT.github.io/static/js/2.11df2935.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherT.github.io/static/js/2.11df2935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff715af70a4f749a6895",
+    "url": "/WeatherT.github.io/static/js/main.c5dbe6d5.chunk.js"
+  },
+  {
+    "revision": "831561ce2e1504019f90",
+    "url": "/WeatherT.github.io/static/js/runtime-main.c2f3cf3c.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/WeatherT.github.io/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/WeatherT.github.io/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
